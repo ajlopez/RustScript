f8cc0dc943b7ef02461ef2bf209992e8e2279185
ajlopez <webmaster@ajlopez.com>
2014-03-15 14:29:26 -0300

    Additional test, parse integer with spaces

--- test/parser.js
+++ test/parser.js
@@ -12,3 +12,15 @@ exports['parse integer number'] = function (test) {
     
     test.equal(parser.parse("Integer"), null);    
 };
+
+exports['parse integer number with spaces'] = function (test) {
+    var parser = parsers.createParser('  123   ');
+    
+    var result = parser.parse("Integer");
+    
+    test.ok(result);
+    test.equal(result.value.evaluate(null), 123);
+    test.equal(result.type, "Integer");
+    
+    test.equal(parser.parse("Integer"), null);    
+};
