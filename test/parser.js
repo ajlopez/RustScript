17fb8f772acd00698d7ff1388191f900aa0e312e
ajlopez <webmaster@ajlopez.com>
2014-03-15 14:39:15 -0300

    Parse name, first test

--- test/parser.js
+++ test/parser.js
@@ -1,5 +1,6 @@
 
 var parsers = require('../lib/parser');
+var contexts = require('../lib/context');
 
 exports['parse integer number'] = function (test) {
     var parser = parsers.createParser('123');
@@ -25,14 +26,17 @@ exports['parse integer number with spaces'] = function (test) {
     test.equal(parser.parse("Integer"), null);    
 };
 
-exports['parse integer number'] = function (test) {
-    var parser = parsers.createParser('123');
+exports['parse name'] = function (test) {
+    var parser = parsers.createParser('foo');
     
-    var result = parser.parse("Integer");
+    var result = parser.parse("Name");
     
     test.ok(result);
-    test.equal(result.value.evaluate(null), 123);
-    test.equal(result.type, "Integer");
     
-    test.equal(parser.parse("Integer"), null);    
+    var context = contexts.createContext();
+    
+    test.equal(result.value.evaluate(context), null);
+    test.equal(result.type, "Name");
+    
+    test.equal(parser.parse("Name"), null);    
 };
