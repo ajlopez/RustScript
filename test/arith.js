9592ade703ccb2bea554d1b192e087f8ea3ee400
ajlopez <webmaster@ajlopez.com>
2014-03-16 13:22:39 -0300

    Arithmetic operation, first tests

---
+++ test/arith.js
@@ -0,0 +1,33 @@
+
+var parsers = require('../lib/parser');
+
+function evaluate(text) {
+    var parser = parsers.createParser(text);
+    var expr = parser.parse('Expression');
+    return expr.value.evaluate(null);
+};
+
+exports['parse and evaluate integer'] = function (test) {
+    test.equal(evaluate('42'), 42); 
+};
+
+exports['parse and evaluate add integers'] = function (test) {
+    test.equal(evaluate('1+2'), 3);
+};
+
+exports['parse and evaluate substract integers'] = function (test) {
+    test.equal(evaluate('1-2'), -1);
+};
+
+exports['parse and evaluate multiply integers'] = function (test) {
+    test.equal(evaluate('2*3'), 6);
+};
+
+exports['parse and evaluate divide integers'] = function (test) {
+    test.equal(evaluate('6/2'), 3);
+};
+
+exports['parse and evaluate simple arithmetic expression'] = function (test) {
+    test.equal(evaluate('2+6/2'), 5);
+};
+
