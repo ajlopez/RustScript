957ffa1c88e5bed1e653e62f00ac2db8efdb69ba
ajlopez <webmaster@ajlopez.com>
2014-03-19 07:08:05 -0300

    Context getValue using parent context

--- test/context.js
+++ test/context.js
@@ -14,3 +14,15 @@ exports['set and get local value'] = function (test) {
     test.equal(context.hasLocalValue('foo'), true);
     test.equal(context.getLocalValue('foo'), 'bar');
 };
+
+exports['get value from parent'] = function (test) {
+    var parent = contexts.createContext();
+    var context = contexts.createContext(parent);
+    
+    parent.setLocalValue('foo', 'bar');
+    test.equal(context.getValue('foo'), 'bar');
+    test.equal(context.hasLocalValue('foo'), false);
+    test.equal(context.getLocalValue('foo'), null);
+    test.equal(parent.hasLocalValue('foo'), true);
+    test.equal(parent.getLocalValue('foo'), 'bar');
+};
