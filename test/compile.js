bc81eea516252ff1102d5a4f2352aab3235fa630
ajlopez <webmaster@ajlopez.com>
2014-03-20 08:15:43 -0300

    Fix program as list of commands

--- test/compile.js
+++ test/compile.js
@@ -9,3 +9,12 @@ exports['compile simple function'] = function (test) {
     
     test.equal(result(), 42);
 };
+
+exports['compile two simple function'] = function (test) {
+    var context = rustscript.createContext();
+    test.ok(rustscript.compile('fn one() { 1 } fn main() { 42 }', context));
+    
+    var result = context.getLocalValue('main');
+    
+    test.equal(result(), 42);
+};
