a4f4c17eda6c34ffe7300ea4af50f374fbb2d63f
ajlopez <webmaster@ajlopez.com>
2014-03-20 08:17:07 -0300

    Refactor name expression, factorial1 sample

---
+++ samples/simple/factorial1.rs
@@ -0,0 +1,19 @@
+
+fn factorial(n) {
+    if n == 1 {
+        1
+    }
+    else {
+        n * factorial(n - 1)
+    }
+}
+
+fn main() {
+    let n = 1;
+    
+    while n <= 10 {
+        println!(factorial(n));
+        n = n + 1;
+    }
+}
+
