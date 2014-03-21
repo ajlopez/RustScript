a92c44b27c223e946b9b5cd536be768dc3ba58a2
ajlopez <webmaster@ajlopez.com>
2014-03-21 08:17:36 -0300

    Test set undefined or immutable name

--- test/context.js
+++ test/context.js
@@ -7,22 +7,57 @@ exports['has local value of unknown variablle'] = function (test) {
     test.equal(context.hasLocalValue('foo'), false);
 };
 
-exports['set and get local value'] = function (test) {
+exports['define and get local value'] = function (test) {
     var context = contexts.createContext();
     
+    context.defineLocalValue('foo', 'bar');
+    test.equal(context.hasLocalValue('foo'), true);
+    test.equal(context.getLocalValue('foo'), 'bar');
+};
+
+exports['define mutable, set value and get local value'] = function (test) {
+    var context = contexts.createContext();
+    
+    context.defineLocalValue('foo', 'none', true);
     context.setLocalValue('foo', 'bar');
     test.equal(context.hasLocalValue('foo'), true);
     test.equal(context.getLocalValue('foo'), 'bar');
+};
+
+exports['raise if set undefined name'] = function (test) {
+    var context = contexts.createContext();
+    
+    test.throws(function () {
+        context.setLocalValue('foo', 'bar');
+    },
+    function (err) {
+        test.equal(err, "undefined 'foo'");
+        return true;
+    });
+};
+
+exports['raise if modify immutable value'] = function (test) {
+    var context = contexts.createContext();
+    context.defineLocalValue('foo', 'none');
+    
+    test.throws(function () {
+        context.setLocalValue('foo', 'bar');
+    },
+    function (err) {
+        test.equal(err, "immutable 'foo'");
+        return true;
+    });
 };
 
 exports['get value from parent'] = function (test) {
     var parent = contexts.createContext();
     var context = contexts.createContext(parent);
     
-    parent.setLocalValue('foo', 'bar');
+    parent.defineLocalValue('foo', 'bar');
     test.equal(context.getValue('foo'), 'bar');
     test.equal(context.hasLocalValue('foo'), false);
     test.equal(context.getLocalValue('foo'), null);
     test.equal(parent.hasLocalValue('foo'), true);
     test.equal(parent.getLocalValue('foo'), 'bar');
 };
+
