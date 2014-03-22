9e9f59f95225d14abba0ba4c2959d7f1dba00af8
ajlopez <webmaster@ajlopez.com>
2014-03-22 10:01:31 -0300

    Module operator %

--- test/arith.js
+++ test/arith.js
@@ -27,6 +27,11 @@ exports['parse and evaluate divide integers'] = function (test) {
     test.equal(evaluate('6/2'), 3);
 };
 
+exports['parse and evaluate mod integers'] = function (test) {
+    test.equal(evaluate('7 % 3'), 1);
+    test.equal(evaluate('6 % 2'), 0);
+};
+
 exports['parse and evaluate simple arithmetic expression'] = function (test) {
     test.equal(evaluate('2+6/2'), 5);
 };
