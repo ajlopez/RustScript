310ab068a3baab997fe78b6149a51da9315f6921
ajlopez <webmaster@ajlopez.com>
2014-03-23 17:38:48 -0300

    Bitwise or, and precedence

--- test/bitwise.js
+++ test/bitwise.js
@@ -12,13 +12,20 @@ function evaluate(text, test) {
     return value;
 };
 
-exports['parse and evaluate bit wise or'] = function (test) {
+exports['parse and evaluate bitwise or'] = function (test) {
     test.equal(evaluate('1 | 2', test), 3); 
 };
 
-exports['parse and evaluate bit wise and'] = function (test) {
+exports['parse and evaluate bitwise and'] = function (test) {
     test.equal(evaluate('7 & 5', test), 5); 
     test.equal(evaluate('1 & 2', test), 0); 
     test.equal(evaluate('7 & 3', test), 3); 
     test.equal(evaluate('7 & 11', test), 3); 
+};
+
+exports['parse and evaluate bitwise and/or'] = function (test) {
+    test.equal(evaluate('4 | 7 & 5', test), 5); 
+    test.equal(evaluate('1 & 2 | 5', test), 5); 
+    test.equal(evaluate('4 | 7 & 3', test), 7); 
+    test.equal(evaluate('4 | 7 & 11', test), 7); 
 };
