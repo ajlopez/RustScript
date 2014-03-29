00498e34245a914a2bf058fe528a56ec8ffcc953
ajlopez <webmaster@ajlopez.com>
2014-03-29 16:46:59 -0300

    Parse function as public

--- test/fn.js
+++ test/fn.js
@@ -2,9 +2,10 @@
 var parsers = require('../lib/parser');
 var contexts = require('../lib/context');
 
-function parse(text) {
+function parse(text, context) {
     var parser = parsers.createParser(text);
-    var context = contexts.createContext();
+    if (!context)
+        context = contexts.createContext();
     return parser.parse('Function').value.evaluate(context);
 }
 
@@ -50,4 +51,29 @@ exports['parse and evaluate function with two parameters'] = function (test) {
     test.equal(fn(0, 1), 1);
     test.equal(fn(1, 2), 3);
     test.equal(fn(2, 4), 6);
+};
+
+exports['parse and get function in context'] = function (test) {
+    var context = contexts.createContext();
+    var fn = parse('fn add(x, y) { x + y }', context);
+    
+    test.ok(fn);
+    test.equal(typeof fn, 'function');
+    
+    test.strictEqual(fn, context.getLocalValue('add'));
+    test.equal(context.hasLocalValue('add'), true);
+    test.equal(context.hasPublicValue('add'), false);
+};
+
+exports['parse and get public function in context'] = function (test) {
+    var context = contexts.createContext();
+    var fn = parse('pub fn add(x, y) { x + y }', context);
+    
+    test.ok(fn);
+    test.equal(typeof fn, 'function');
+    
+    test.strictEqual(fn, context.getLocalValue('add'));
+    test.strictEqual(fn, context.getPublicValue('add'));
+    test.equal(context.hasLocalValue('add'), true);
+    test.equal(context.hasPublicValue('add'), true);
 };
