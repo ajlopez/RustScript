da52d250233bc2ea3a3bcb1ddc6c3a99377fa6da
ajlopez <webmaster@ajlopez.com>
2019-06-07 19:32:56 -0300

    Test refactors

--- test/loop.js
+++ test/loop.js
@@ -1,10 +1,11 @@
 
-var parsers = require('../lib/parser'),
-    contexts = require('../lib/context');
+const parsers = require('../lib/parser');
+const contexts = require('../lib/context');
 
 function execute(text, context, test) {
-    var parser = parsers.createParser(text);
-    var expr = parser.parse('Expression');
+    const parser = parsers.createParser(text);
+    const expr = parser.parse('Expression');
+    
     expr.value.evaluate(context);
     
     if (test)
@@ -12,17 +13,23 @@ function execute(text, context, test) {
 };
 
 exports['simple loop with break'] = function (test) {
-    var context = contexts.createContext();
+    const context = contexts.createContext();
+    
     context.defineLocalValue('a', 1, { mutable: true });
+    
     execute('loop { a = a + 1; if a == 5 { break; } }', context, test);
+    
     test.equal(context.getLocalValue('a'), 5);
 };
 
 exports['simple loop with continue and break'] = function (test) {
-    var context = contexts.createContext();
+    const context = contexts.createContext();
     context.defineLocalValue('a', 0, { mutable: true });
     context.defineLocalValue('b', 0, { mutable: true });
+    
     execute('loop { a = a + 1; if a == 5 { continue; } b = b + a; if (a == 10) { break; } }', context, test);
+    
     test.equal(context.getLocalValue('a'), 10);
     test.equal(context.getLocalValue('b'), 50);
 };
+
--- test/match.js
+++ test/match.js
@@ -1,12 +1,12 @@
 
-var parsers = require('../lib/parser'),
-    rustscript = require('..'),
-    contexts = require('../lib/context');
+const parsers = require('../lib/parser');
+const rustscript = require('..');
+const contexts = require('../lib/context');
 
 function evaluate(text, context, test) {
-    var parser = parsers.createParser(text);
-    var expr = parser.parse('Expression');
-    var result = expr.value.evaluate(context);
+    const parser = parsers.createParser(text);
+    const expr = parser.parse('Expression');
+    const result = expr.value.evaluate(context);
     
     if (test)
         test.equal(parser.parse('Expression'), null);
@@ -15,13 +15,16 @@ function evaluate(text, context, test) {
 };
 
 exports['simple match'] = function (test) {
-    var context = rustscript.createContext();
-    var result = evaluate('match 1 { 1 => "one", 2 => "two" }', context, test);
+    const context = rustscript.createContext();
+    const result = evaluate('match 1 { 1 => "one", 2 => "two" }', context, test);
+    
     test.equal(result, 'one');
 };
 
 exports['match using default'] = function (test) {
-    var context = rustscript.createContext();
-    var result = evaluate('match 10 { 1 => "one", 2 => "two", _ => "other" }', context, test);
+    const context = rustscript.createContext();
+    const result = evaluate('match 10 { 1 => "one", 2 => "two", _ => "other" }', context, test);
+    
     test.equal(result, 'other');
 };
+
